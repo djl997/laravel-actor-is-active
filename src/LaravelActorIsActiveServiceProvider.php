c154f3e43e7fb1d8cf2b3e31ceefbbcc106c4c8f
djl997 <27086857+djl997@users.noreply.github.com>
2023-07-03 10:02:55 +0200

    Composer init

---
+++ src/LaravelActorIsActiveServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Djl997\LaravelActorIsActive;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelActorIsActiveServiceProvider extends ServiceProvider {
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    { }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    { }
+
+}
